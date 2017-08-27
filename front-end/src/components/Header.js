8241488bf6a0a4b0d7c3f366ecabf5be8866f236
Matthew Reishus <mreishus@users.noreply.github.com>
2017-08-27 18:23:16 -0500

    Remove comment

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -8,18 +8,4 @@ const Header = () => (
   </div>
 );
 
-/*
-class Header extends Component {
-  render() {
-    return (
-      <div className="tl  pa2 pa4-ns bg-light-green black">
-        <h1 className="f4 fw6 f1-ns lh-title measure mt0 mb0">
-          Search
-        </h1>
-      </div>
-    );
-  }
-}
-*/
-
 export default Header;
