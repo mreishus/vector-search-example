ee0e5f1afac6736a3ab329596179df2cffecfd77
Matthew Reishus <mreishus@users.noreply.github.com>
2018-02-01 19:42:13 -0600

    SearchTextExpanded doesn't appear until you type

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,12 +1,11 @@
 // @flow
-import React from 'react';
+import React from "react";
 
 const Header = () => (
   <div className="tl pa2 pa4-ns bg-light-green black">
-    <h1 className="f4 fw6 f1-ns lh-title measure mt0 mb0">
-      Search
-    </h1>
-    Search the company names and descriptions of S&amp;P500 companies, powered with a conceptual search.
+    <h1 className="f4 fw6 f1-ns lh-title measure mt0 mb0">Search</h1>
+    Search the company names and descriptions of S&amp;P500 companies, powered
+    with a conceptual search.
   </div>
 );
 
--- front-end/src/components/SearchTextExpanded.js
+++ front-end/src/components/SearchTextExpanded.js
@@ -8,6 +8,9 @@ type Props = {
 class SearchTextExpanded extends React.Component<Props> {
   render() {
     const { searchTextExpanded } = this.props;
+    if (searchTextExpanded == "") {
+      return null;
+    }
     return (
       <div class="pa3 bt bg-light-green br3">
         <p class="f6 f5-ns lh-copy measure mv0">{searchTextExpanded}</p>
